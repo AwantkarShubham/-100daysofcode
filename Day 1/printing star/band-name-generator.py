8894d916db6ae1db6154f20ee4be852c59fb71ac
Shubham Awantkar <awantkarshubham99@gmail.com>
2021-03-14 21:25:26 +0530

    Day 1
    Completed Day 1 with python

---
+++ Day 1/printing star/band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+City=input("What's name of the city you grew up in?")
+pet=input("what's your pet's name?")
+print("Your band name could be "+City+" "+pet)
---
+++ Day 1/printing star/main.py
@@ -0,0 +1,34 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')") 
+
+#String formating using print function
+
+print("Hello world! \nHello world! \nHello world! \n")
+
+#concatenation of string with print function
+
+print("Hello"+" "+"Programmer")
+
+#Input function
+#input() will get user input in console
+#then print() will print the word "Hello" and the user input
+#print("Hello "+input("What is your name?"))
+
+#variables
+'''
+name=input("Enter name:")
+length=len(name)
+print(name)
+print(length)
+'''
+
+a=input("a:")
+b=input("b:")
+
+a,b=b,a
+
+print("a = "+a)
+print("b = "+b)
+
+ 
